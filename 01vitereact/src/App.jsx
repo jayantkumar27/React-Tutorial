152da4322a0f6acbc857719d6aa22a048a97bcb4
jayantaryan <jayantaryan2756@gmail.com>
2026-06-28 15:04:15 +0530

    Components And Format

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
-
+import King from './King.jsx'
 function App() {
 
 
   return (
+    <>
     <h1>React Js Started Learning | Jayant codemen</h1>
+    <King />
+    </>
   )
 }
 
---
+++ 01vitereact/src/King.jsx
@@ -0,0 +1,17 @@
+function King (){
+
+
+    return(
+        <h2>King is CodeGuy</h2>
+    )
+}
+
+export default King
+
+/*
+IN React
+Function like this = Component
+
+and hume function ka naam Capital letter se likhna hai kyoki ye React ka RULE hai
+
+*/
