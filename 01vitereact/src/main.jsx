2ceb19bfb4fbbe2fc8390112bd0b64211fa41d35
jayantaryan <jayantaryan2756@gmail.com>
2026-06-30 12:13:59 +0530

    SPA Knowledge

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -12,4 +12,16 @@ createRoot(document.getElementById('root')).render(
 
 /*
 document.getElementById('root')  isse hum index.html ke andar se root id ke refference lete hai
+phir usko react ka createRoot karte hai phir root variable mai store kara sakte hai ya phir seedha
+refference deke React ka render use kar sakte hai
+
+
+isse hi Phir humara Virtual Dom Banata 
+
+aur isko hi use karke hum page mai cheezo ko add karte hai aur nikalte hai 
+jisse lagta hai ki hum alag alag page pe jaate hai 
+like in url /profile ya /Contacts ya /Skill
+
+Isko hi SPA kahte hai 
+Single Page Application
  */
