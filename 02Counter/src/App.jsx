e1939988d485cde9e251ceb7049c3d924f8e7c13
jayantaryan <jayantaryan2756@gmail.com>
2026-07-04 14:15:04 +0530

    Project FrontPage Ready

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -6,10 +6,15 @@ import './App.css'
 
 function App() {
  
+  const counter =15
+ 
 
   return (
     <>
-     
+     <h1>React Tutorial</h1>
+     <h2>Number Counter = {counter}</h2>
+     <button >Add one Number</button>
+     <button >Subract one Number</button>
     </>
   )
 }
