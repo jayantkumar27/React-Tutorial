953d3b2073644f46cd3ed02b45dea0d0516c606c
jayantaryan <jayantaryan2756@gmail.com>
2026-07-16 23:59:28 +0530

    ui 1

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
     <div className='w-full h-screen duration-200'
      style={{backgroundColor:color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-s-0 px-2'>
-         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+         <div className='flex  justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
               <button
               onClick={()=> setColor("red")}
-              className='outline-none px-4 py-1 rounded-full text-black shadow-lg'>
+              className='outline-none px-4 py-1 rounded-full text-red-500 shadow-lg'>
                 Red
               </button>
               <button
