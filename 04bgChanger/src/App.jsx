1242b38997176a9bca306acc842e66f962b1f7e5
jayantaryan <jayantaryan2756@gmail.com>
2026-07-16 23:45:58 +0530

    Making progress

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -3,12 +3,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("grey")
 
   return (
     <>
-    <div className='w-full h-screen duration-200'>
+    <div className='w-full h-screen duration-200'
+     style={{backgroundColor:color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-s-0 px-2'>
 
+        </div>
     </div>
     </>
   )
