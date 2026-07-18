edc3251278c8376d96981996eaf8f46ba220e88b
jayantaryan <jayantaryan2756@gmail.com>
2026-07-18 21:41:25 +0530

    Intial setup complete

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
    <>
    <div className='w-full flex justify-center'>
 
-   <h1 className='inline-block bg-amber-500 text-2xl px-3 py-1.5'>The CodeGuy</h1>
+   <h1 className='inline-block bg-amber-500 text-2xl px-3 py-1.5 rounded-3xl'>The CodeGuy</h1>
    </div>
    </>
   )
