1fb809fd9287120091c087ef50a18104be6701e3
jayantaryan <jayantaryan2756@gmail.com>
2026-07-22 20:09:42 +0530

    updating UI'

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -31,6 +31,13 @@ function App() {
    
    >The CodeGuy &lt;/&gt;   </h1>
 
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+    <h1 className='text-white text-center my-3'>Password Generator</h1>
+    <div className='flex shadoe rounded-lg overflow-hidden mb-4'>
+         
+    </div>
+   </div>
+
    
    </div>
  
