01f99776968c95653b304037b1dcd9e9f08cb757
jayantaryan <jayantaryan2756@gmail.com>
2026-07-22 20:03:53 +0530

    Function Completed

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -13,9 +13,13 @@ function App() {
     if (numberallowed) str+="1234567890"
     if (charallowed) str+="!@#$%^&*-_+=[]{}~`"
     
-    
+    for (let i=1 ; i<= length; i++){
+      let char = Math.floor(Math.random()*str.length +1)
+      pass+= str.charAt(char)
+    }
+    setpasswrd(pass)
 
-  })
+  },[length,numberallowed,charallowed,setpasswrd])
 
   return (
    <>
