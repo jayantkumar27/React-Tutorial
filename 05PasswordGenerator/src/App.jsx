1bb16e8172b5067d6cd9796fdb3161f579389a20
jayantaryan <jayantaryan2756@gmail.com>
2026-07-22 20:20:46 +0530

    updating UI-2

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -34,7 +34,31 @@ function App() {
    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
     <h1 className='text-white text-center my-3'>Password Generator</h1>
     <div className='flex shadoe rounded-lg overflow-hidden mb-4'>
-         
+         <input 
+         type="text" 
+         value={passwrd}
+         className='outline-none w-full py-1 px-3 bg-gray-400 text-white'
+         placeholder='Password'
+         readOnly
+         />
+         <button 
+         className=' outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+          COPY
+         </button>
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range" 
+        min={6}
+        max={100}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e)=>{setLength(e.target.value)}}
+        />
+        <label >Length:{length}</label>
+
+      </div>
     </div>
    </div>
 
