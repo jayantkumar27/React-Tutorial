da015bd383bac4d8db4ca66bc0f7d915f42562d9
jayantaryan <jayantaryan2756@gmail.com>
2026-08-08 01:29:59 +0530

    Currency Convertor Completed on surface

--- 06CurrencyConvertor/src/App.jsx
+++ 06CurrencyConvertor/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [convertedAmount, setConvertedAmount] = useState(0)
 
   const currencyInfo = useCurrencyInfo(from)
-
+  
   const options = Object.keys(currencyInfo)
 
   const swap = () => {
@@ -25,11 +25,13 @@ function App() {
     setConvertedAmount(amount * currencyInfo[to])
   }
 
-   return (
-        <div
-            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+  return (
+    <>
+   
+    <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-amber-100"
             style={{
-                backgroundImage: `url('${BackgroundImage}')`,
+                backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
             }}
         >
             <div className="w-full">
@@ -37,12 +39,18 @@ function App() {
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
+                            convert()
                            
                         }}
                     >
                         <div className="w-full mb-1">
                             <InputBox
                                 label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setAmount(amount) }
+                                selectCurrency={from}
+                                onAmountChange={(amount) => setAmount(amount)}
                                 
                             />
                         </div>
@@ -50,25 +58,31 @@ function App() {
                             <button
                                 type="button"
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                                
-                            >
+                                onClick={swap}
+                                >
                                 swap
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
                             <InputBox
                                 label="To"
+                                amount={convertedAmount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setTo(currency)}
+                                selectCurrency={from}
+                                amountDisable
                                 
                             />
                         </div>
                         <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                            Convert 
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
                     </form>
                 </div>
             </div>
         </div>
-    );
+    </>
+  );
 }
 
 export default App 
